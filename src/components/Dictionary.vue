6311a88de1c0ed3419f35647173bcb72f7dbc252
Igor Dubinin <i.dubinin@x5.ru>
2024-09-05 17:02:50 +0400

    html

--- src/components/Dictionary.vue
+++ src/components/Dictionary.vue
@@ -1,57 +1,49 @@
 <script setup lang="ts">
-    import * as _ from 'lodash';
-    import wordsList from '../data/translated';
+import * as _ from "lodash";
+import wordsList from "../data/translated";
 
-    const limit = 100;
-    const optionsLimit = 4;
+const limit = 100;
+const optionsLimit = 4;
 
-    let words = _.shuffle(_.clone(wordsList));
+let words = _.shuffle(_.clone(wordsList));
 
-    const lesson = [];
-    let i = 0;
-    let j = limit;
+const lesson = [];
+let i = 0;
+let j = limit;
 
-    while (i < limit) {
-      const word = words[i];
-      const options = [word];
+while (i < limit) {
+  const word = words[i];
+  const options = [word];
 
-      for (let oI = 1; oI < optionsLimit; ++oI) {
-        options.push(words[j]);
-        j += 1;
-      }
+  for (let oI = 1; oI < optionsLimit; ++oI) {
+    options.push(words[j]);
+    j += 1;
+  }
 
-      lesson.push({
-        word,
-        options,
-      });
+  lesson.push({
+    word,
+    options,
+  });
 
-
-      i += 1
-    }
-
-
+  i += 1;
+}
 </script>
 
 <template>
-  <div>
+  <div class="dictionary">
     <h3>Lesson</h3>
 
-    <ul>
-      <li v-for="item in lesson" :key="item.word.id">
-        <b>{{ item.word.id }} - {{ item.word.en }} - {{ item.word.ru }}</b>
-
-        <ul>
-          <li v-for="option in item.options" :key="option.id">
+    <div class="lessons" v-for="item in lesson" :key="item.word.id">
+      <div class="lesson-item">
+        <h3>{{ item.word.id }} - {{ item.word.en }} - {{ item.word.ru }}</h3>
+        <div class="card-wrapper">
+          <div class="card" v-for="option in item.options" :key="option.id">
             <b>{{ option.id }} - {{ option.en }} - {{ option.ru }}</b>
-
-          </li>
-        </ul>
-
-      </li>
-    </ul>
-
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
